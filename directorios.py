d0bfc8d11d712c636f5a653a1759b99662371e0e
Víctor Torres <vtorres@cnt.lat>
2024-11-26 11:32:10 -0600

    Update directorios.py
    # Remove os function to work with OneDrive. Connect to folders through normpaths.

--- directorios.py
+++ directorios.py
@@ -1,16 +1,12 @@
 import os
 import pandas as pd
-# --------------------------------------------------------------------------------------------------------------------------------------
-#                                                        # ONEDRIVE FOR EVERYONE                                                        
-# --------------------------------------------------------------------------------------------------------------------------------------
-one_drive = os.getenv("OneDrive")
 
-# --------------------------------------------------------------------------------------------------------------------------------------
-#                                                        # INPUT PATH AND FILES                                                        
-# --------------------------------------------------------------------------------------------------------------------------------------
-data_folder_path = os.path.join(one_drive, os.path.normpath(r"CNT\4. Proyectos\8. Interfaz de descarga de datos (Experimental)\2. Datos"))
+# ----------------------------------------------------------------------
+#                          INPUT PATH AND FILES
+# ----------------------------------------------------------------------
+data_folder_path = os.path.normpath(r"data")
 
-single_well_logs_paths = os.path.join(one_drive, os.path.normpath(r"CNT\4. Proyectos\8. Interfaz de descarga de datos (Experimental)\2. Datos\single_well_logs"))
+single_well_logs_paths = os.path.join(data_folder_path, "single_well_logs")
 
 log_file = "logs.xlsx"
 log_path_and_file = os.path.join(data_folder_path, log_file)
@@ -26,6 +22,7 @@ npt_path_file = os.path.join(data_folder_path, npt_file)
 
 inpt_file = "inpt.xlsx"
 inpt_path_file = os.path.join(data_folder_path, inpt_file)
-# --------------------------------------------------------------------------------------------------------------------------------------
-#                                                        # OUTPUT PATH AND FILES                                                        
-# --------------------------------------------------------------------------------------------------------------------------------------
+
+# ----------------------------------------------------------------------
+#                          OUTPUT PATH AND FILES
+# ----------------------------------------------------------------------
